8195f9408fa71f32f8b86db5326e74df219bd518
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2025-03-13 15:43:20 -0300

    finalizado

--- indb.py
+++ indb.py
@@ -1,10 +1,8 @@
 from product import Product
+import json
 
 def generate_products():
-    list_products = []
-
-    for x in range(10):
-        p = Product(name=f'Product {x}', price = 4.90 * x)
-        list_products.append(p)
-    
-    return list_products
+    f = open('./data/products.json')
+    data = json.loads(f.read())
+    f.close
+    return data
